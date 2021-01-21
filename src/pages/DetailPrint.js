8a2bea71da3dccb328f5d8b32b321f6aadb286c9
DDobrowolski <damian.dobrowolski99@gmail.com>
2021-01-21 17:54:18 +0100

    Code clean

--- src/pages/DetailPrint.js
+++ src/pages/DetailPrint.js
@@ -14,10 +14,6 @@ const mainHeadCells = [
     disablePadding: false,
     label: 'Sposób przyrządzenia',
   },
-  // { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  // { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  // { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  // { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
 ];
 
 const ingredientsHeadCells = [
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,10 +9,6 @@ const headCells = [
   { id: 'name', numeric: false, disablePadding: true, label: 'Nazwa' },
   { id: 'created_at', numeric: false, disablePadding: true, label: 'Dodano' },
   { id: 'description', numeric: false, disablePadding: true, label: 'Opis' },
-  // { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  // { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  // { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  // { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
 ];
 
 const fetchRecipes = async (
